366aeb65fd27f01f9ea658d5be25272b1f1eb44c
Raul Andrade <theandraderaul@gmail.com>
2021-02-06 23:16:52 -0300

    fix: remove .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/characters":["static\u002Fchunks\u002Fpages\u002Fcharacters.js"],"/teams/[id]":["static\u002Fchunks\u002Fpages\u002Fteams\u002F[id].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcharacters","\u002Fteams\u002F[id]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
