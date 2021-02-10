47bbf5638ebcb7bfc2e938cc53404a4513fa0851
Raul Andrade <theandraderaul@gmail.com>
2021-02-09 21:06:46 -0300

    feat: search team

--- lib/teams.ts
+++ lib/teams.ts
@@ -1,7 +1,7 @@
-import api from '../services/api';
-import ITeam from '../interfaces/team';
-import { GET } from '../constants/verbs';
-import { TEAMS } from '../constants/endpoints';
+import api from "../services/api";
+import ITeam from "../interfaces/team";
+import { GET } from "../constants/verbs";
+import { TEAMS } from "../constants/endpoints";
 
 export async function getTeams(): Promise<ITeam[]> {
   try {
@@ -15,6 +15,30 @@ export async function getTeams(): Promise<ITeam[]> {
   } catch (error) {
     //TODO: throw error
     console.log(error);
+    return [];
+  }
+}
+
+export async function getTeamsByParams(
+  params: string,
+  letter: string
+): Promise<ITeam[]> {
+  try {
+    const {
+      data: { data },
+    } = await api({
+      method: GET,
+      url: `${TEAMS}?${params}=${letter}`,
+    });
+    return data;
+  } catch (error) {
+    //TODO: throw error
+    if (error.code === "ECONNREFUSED") {
+      console.error("Server is down");
+    } else {
+      console.error(error);
+      throw error;
+    }
     return [];
   }
 }
--- pages/teams.tsx
+++ pages/teams.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
-import { getTeams } from '../lib/teams';
-import ITeam from '../interfaces/team';
-import { LINKS } from '../constants/urls';
-import { TEAMS } from '../constants/endpoints';
-import Grid from '../components/Grid';
-import Card from '../components/Card';
-import Search from '../components/Search';
-import Alphabet from '../components/Alphabet';
+import React, { useEffect, useState } from "react";
+import { getTeams, getTeamsByParams } from "../lib/teams";
+import ITeam from "../interfaces/team";
+import { LINKS } from "../constants/urls";
+import { TEAMS } from "../constants/endpoints";
+import Search from "../components/Search";
+import Alphabet from "../components/Alphabet";
+import useAsync, { RequestStatus } from "../hooks/useAsync";
+import { useRouter } from "next/router";
+import ContentList, {
+  IDataContent,
+} from "../components/ContentList/ContentList";
+import LoadingSharingan from "../components/LoadingSharingan";
 
 interface IPropsTeam {
   data: ITeam[];
@@ -22,22 +26,49 @@ export async function getStaticProps() {
 }
 
 const Teams: React.FC<IPropsTeam> = ({ data }: IPropsTeam) => {
-  const [letter, setLetter] = useState('');
+  const { status, error, data: dataAsync, runPromise } = useAsync({
+    status: RequestStatus.RESOLVED,
+    data: data,
+  });
+
+  const [letter, setLetter] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const { query } = router;
+    if (query.from) {
+      const from = query.from as string;
+      setLetter(from);
+      return runPromise(getTeamsByParams("from", from));
+    } else if (query.name) {
+      const name = query.name as string;
+      setLetter("");
+      return runPromise(getTeamsByParams("name", name));
+    }
+  }, [router]);
+
+  const RenderContent = () => {
+    const content = {
+      [RequestStatus.PENDING as string]: <LoadingSharingan />,
+      [RequestStatus.RESOLVED as string]: (
+        <ContentList
+          data={dataAsync as IDataContent[]}
+          noContent="Team not found"
+          onClick={LINKS.team}
+        />
+      ),
+      [RequestStatus.REJECTED as string]: <div>{error?.message}</div>,
+      default: <div>Unhandled status: {status}</div>,
+    };
+
+    return content[status ?? "default"];
+  };
+
   return (
     <>
       <Search pathname={TEAMS} />
       <Alphabet pathname={TEAMS} letter={letter} setLetter={setLetter} />
-      <Grid>
-        {data.map((item) => (
-          <Card
-            key={item.id}
-            src={item.picture}
-            name={item.name}
-            id={item.id}
-            onClick={LINKS.team}
-          />
-        ))}
-      </Grid>
+      <RenderContent />
     </>
   );
 };
