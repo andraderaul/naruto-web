fb88439bed0e2175cf89d7e216956a43f1950600
Raul Andrade <theandraderaul@gmail.com>
2021-02-10 23:06:07 -0300

    feat: test in the component header

--- components/Header/Header.spec.tsx
+++ components/Header/Header.spec.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+import { screen, waitFor } from "@testing-library/react";
+import { render } from "../../util/mock/router.mock";
 import Header from ".";
 
 describe("<Header/>", () => {
@@ -8,5 +8,9 @@ describe("<Header/>", () => {
     render(<Header />);
     const nodeHeader = await waitFor(() => screen.getByTestId("header"));
     expect(nodeHeader).toBeTruthy();
+
+    const nodeLink = await waitFor(() => screen.getByText("Characters"));
+    expect(nodeLink).toBeTruthy();
+    userEvent.click(nodeLink);
   });
 });
