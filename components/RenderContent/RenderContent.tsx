d1b06f41f917919d9e0a2a47d4dc1b71670719bb
Raul Andrade <theandraderaul@gmail.com>
2021-03-08 10:51:09 -0300

    feat: add component RenderContent

---
+++ components/RenderContent/RenderContent.tsx
@@ -0,0 +1,32 @@
+import LoadingSharingan from '../LoadingSharingan';
+import ContentList, { IDataContent } from '../ContentList/ContentList';
+import { RequestStatus } from '../../hooks/useAsync';
+
+interface IPropsRenderContent {
+  status?: string;
+  data: IDataContent[];
+  error: string;
+  noContentMessage: string;
+  onClick: Function;
+}
+
+const RenderContent: React.FC<IPropsRenderContent> = ({
+  status,
+  data,
+  error,
+  noContentMessage,
+  onClick,
+}: IPropsRenderContent) => {
+  const content = {
+    [RequestStatus.PENDING as string]: <LoadingSharingan />,
+    [RequestStatus.RESOLVED as string]: (
+      <ContentList data={data} noContent={noContentMessage} onClick={onClick} />
+    ),
+    [RequestStatus.REJECTED as string]: <div>{error}</div>,
+    default: <div>Unhandled status: {status}</div>,
+  };
+
+  return content[status ?? 'default'];
+};
+
+export default RenderContent;
