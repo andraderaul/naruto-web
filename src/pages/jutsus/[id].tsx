45205f0e2b2a621b459f84cbeb7aba3ebb0bc572
Raul Andrade <theandraderaul@gmail.com>
2021-03-24 23:17:48 -0300

    :sparkles: feat: added pages about jutsus and teams

--- src/pages/jutsus/[id].tsx
+++ src/pages/jutsus/[id].tsx
@@ -1,3 +1,60 @@
-const Jutsu = () => <div>jutsu</div>
+import { GetStaticPropsContext } from 'next'
+import PageTitle from '../../components/PageTitle'
+import IJutsu from '../../interfaces/jutsu'
+import { ROUTES } from '../../constants/urls'
+import { getJutsus, getJutsusById } from '../../lib/jutsus'
+
+type PropsJutsu = {
+  data: IJutsu
+}
+
+type StaticPath = {
+  paths: string[]
+  fallback: boolean
+}
+
+type StaticProps = {
+  props: {
+    data: IJutsu | null
+  }
+}
+
+export async function getStaticPaths(): Promise<StaticPath> {
+  const data = await getJutsus()
+  const paths = data.map((jutsus) => `${ROUTES.jutsus}/${jutsus.id}`)
+
+  console.log(paths)
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(
+  context: GetStaticPropsContext
+): Promise<StaticProps> {
+  const { params } = context
+  const id = params?.id as string
+  const data = await getJutsusById(id)
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+const Jutsu = ({ data }: PropsJutsu) => (
+  <>
+    <PageTitle title={data.name} />
+    <div
+      style={{
+        marginTop: '10rem'
+      }}
+    >
+      <p>{data.name}</p>
+    </div>
+  </>
+)
 
 export default Jutsu
--- src/pages/teams/[id].tsx
+++ src/pages/teams/[id].tsx
@@ -1,3 +1,58 @@
-const Team = () => <div>team</div>
+import { GetStaticPropsContext } from 'next'
+import PageTitle from '../../components/PageTitle'
+import ITeam from '../../interfaces/team'
+import { ROUTES } from '../../constants/urls'
+import { getTeams, getTeamsById } from '../../lib/teams'
+
+type PropsTeam = {
+  data: ITeam
+}
+
+type StaticPath = {
+  paths: string[]
+  fallback: boolean
+}
+
+type StaticProps = {
+  props: {
+    data: ITeam | null
+  }
+}
+
+export async function getStaticPaths(): Promise<StaticPath> {
+  const data = await getTeams()
+  const paths = data.map((team) => `${ROUTES.teams}/${team.id}`)
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(
+  context: GetStaticPropsContext
+): Promise<StaticProps> {
+  const { params } = context
+  const id = params?.id as string
+  const data = await getTeamsById(id)
+
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+const Team = ({ data }: PropsTeam) => (
+  <>
+    <PageTitle title={data.name} />
+    <div
+      style={{
+        marginTop: '10rem'
+      }}
+    >
+      <p>{data.name}</p>
+    </div>
+  </>
+)
 
 export default Team
